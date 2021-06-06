6be4800290e1a52c5bb7c3b2add090275fd33f84
Florian Lercher <florian.lercher@tum.de>
2021-06-06 16:57:02 +0200

    Add styled menu

--- src/components/Tiptap.vue
+++ src/components/Tiptap.vue
@@ -1,14 +1,19 @@
 <template>
-  <editor-content :editor="editor" />
+  <div class="editor" v-if="editor">
+    <menu-bar class="editor__header" :editor="editor" />
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
 </template>
 
 <script>
 import { Editor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+import MenuBar from './MenuBar.vue'
 
 export default {
   components: {
     EditorContent,
+    MenuBar,
   },
 
   props: {
@@ -53,3 +58,117 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.editor {
+  display: flex;
+  flex-direction: column;
+  max-height: 400px;
+  color: #0D0D0D;
+  background-color: #FFFFFF;
+  border: 3px solid #0D0D0D;
+  border-radius: 0.75rem;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    flex: 0 0 auto;
+    flex-wrap: wrap;
+    padding: 0.25rem;
+    border-bottom: 3px solid #0D0D0D;
+  }
+
+  &__content {
+    padding: 0.25rem;
+    flex: 1 1 auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+  }
+}
+</style>
+
+<style lang="scss">
+/* Basic editor styles */
+.ProseMirror {
+  &:focus {
+    outline: none;
+  }
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  ul,
+  ol {
+    padding: 0 1rem;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.1;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
+
+  pre {
+    background: #0D0D0D;
+    color: #FFF;
+    font-family: 'JetBrainsMono', monospace;
+    padding: 0.75rem 1rem;
+    border-radius: 0.5rem;
+
+    code {
+      color: inherit;
+      padding: 0;
+      background: none;
+      font-size: 0.8rem;
+    }
+  }
+
+  mark {
+    background-color: #FAF594;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  hr {
+    margin: 1rem 0;
+  }
+
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(#0D0D0D, 0.1);
+  }
+
+  hr {
+    border: none;
+    border-top: 2px solid rgba(#0D0D0D, 0.1);
+    margin: 2rem 0;
+  }
+
+  ul[data-type="taskList"] {
+    list-style: none;
+    padding: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+
+      > label {
+        flex: 0 0 auto;
+        margin-right: 0.5rem;
+      }
+    }
+  }
+}
+</style>
