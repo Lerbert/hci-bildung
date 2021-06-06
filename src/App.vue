1dfcca20571d6d6259373d46dbadd29efb5d6ff9
Florian Lercher <florian.lercher@tum.de>
2021-06-06 19:54:40 +0200

    Convert to ts

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <div id="app">
-    <tiptap v-model="editorContent"></tiptap>
-    Inhalt: {{ editorContent }}
-  </div>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
-import Tiptap from './components/Tiptap.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
+export default defineComponent({
   name: 'App',
-  
   components: {
-    Tiptap
-  },
-
-  data() {
-    return {
-      editorContent: '',
-    }
-  },
-}
+    HelloWorld
+  }
+});
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
