b13f56507a1d3afdab13af3ae8c58a48919e1a9e
Florian Lercher <florian.lercher@tum.de>
2021-06-06 15:51:22 +0200

    Echo changes in editor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <tiptap v-model="editorContent"></tiptap>
+    Inhalt: {{ editorContent }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tiptap from './components/Tiptap.vue'
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
-  }
+    Tiptap
+  },
+
+  data() {
+    return {
+      editorContent: '',
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
