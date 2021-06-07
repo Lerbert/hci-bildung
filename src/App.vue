0532aa711aa2c5ac7a0bf6f1bc4218831ec553b9
Florian Lercher <florian.lercher@tum.de>
2021-06-07 22:27:34 +0200

    Add CSS classes based on correctness

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div id="app">
     <tiptap v-model="editorContent"></tiptap>
     <p>Sicht der Lernenden</p>
-    <div v-html="sheet"></div>
+    <sheet :editorHTML="editorContent"></sheet>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Tiptap from './components/Tiptap.vue';
+import Sheet from './components/Sheet.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     Tiptap,
+    Sheet,
   },
 
   data() {
