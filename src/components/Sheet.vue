af568e67a5388cadd3493e3918fdc68c19a4f017
Florian Lercher <florian.lercher@tum.de>
2021-06-09 18:40:38 +0200

    Remove debug output

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,7 +1,6 @@
 <template>
   <node :tiptapNode="editorJSON" :checkTrigger="checkTrigger"></node>
   <button v-on:click="checkAll">Alle überprüfen</button>
-  <div>{{ editorJSON }}</div>
 </template>
 
 <script lang="ts">
