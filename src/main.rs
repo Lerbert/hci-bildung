722563dbbe295d4617b09d7c0bc7d79cdb4e529c
Florian Lercher <florian.lercher@tum.de>
2022-02-14 02:19:49 +0100

    Add url_for function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
-#[macro_use] extern crate rocket;
-use rocket::fs::{FileServer, relative};
+#[macro_use]
+extern crate rocket;
+use rocket::fs::{relative, FileServer};
 use rocket_dyn_templates::Template;
+use tera::{self, from_value, to_value, Function};
 
 #[get("/")]
 fn index() -> Template {
@@ -16,13 +18,40 @@ fn documents() -> Template {
     Template::render("docmgmt", &context)
 }
 
+fn make_url_for(urls: HashMap<String, String>) -> impl Function {
+    move |args: &HashMap<String, tera::Value>| -> tera::Result<tera::Value> {
+        match args.get("name") {
+            Some(val) => match from_value::<String>(val.clone()) {
+                Ok(v) => Ok(to_value(urls.get(&v).unwrap()).unwrap()),
+                Err(_) => Err("oops".into()),
+            },
+            None => Err("oops".into()),
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-    .mount("/", routes![index, documents])
-    .mount("/vue", FileServer::from(relative!("vue_dist/vue")))
-    .mount("/assets", FileServer::from(relative!("assets")))
-    .attach(Template::custom(|engines| {
-        engines.tera.add_template_file("vue_dist/index.html", Some("vue_index")).ok();
+    let r = rocket::build()
+        .mount("/", routes![index, documents])
+        .mount("/vue", FileServer::from(relative!("vue_dist/vue")))
+        .mount("/assets", FileServer::from(relative!("assets")));
+    let map: HashMap<String, String> = r
+        .routes()
+        .map(|route| {
+            (
+                route.name.clone().unwrap().into(),
+                route.uri.as_str().into(),
+            )
+        })
+        .collect();
+    r.attach(Template::custom(move |engines| {
+        engines
+            .tera
+            .add_template_file("vue_dist/index.html", Some("vue_index"))
+            .ok();
+        engines
+            .tera
+            .register_function("url_for", make_url_for(map.clone()));
     }))
 }
