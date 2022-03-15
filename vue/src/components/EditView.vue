c063d57ebba3bacf574e30ff9327f0f28be9a64d
Florian Lercher <florian.lercher@tum.de>
2022-03-15 19:33:53 +0100

    Add UI for export

--- vue/src/components/EditView.vue
+++ vue/src/components/EditView.vue
@@ -22,7 +22,30 @@
       <div class="column is-half is-flex is-flex-direction-column" id="preview">
         <div class="is-flex is-justify-content-space-between">
           <h1 class="title">Vorschau</h1>
-          <share-button :docId="docId"></share-button>
+          <div class="is-flex is-justify-content-right buttons">
+            <share-button :docId="docId"></share-button>
+            <more-button class="is-right">
+              <a class="dropdown-item icon-text">
+                <span class="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    width="20"
+                    height="20"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>Herunterladen</span>
+              </a>
+            </more-button>
+          </div>
         </div>
         <sheet-display :editorJSON="editorContent"></sheet-display>
       </div>
@@ -34,14 +57,16 @@
 import { defineComponent } from "vue";
 import debounce from "lodash/debounce";
 
+import MoreButton from "./MoreButton.vue";
 import { SaveStatus } from "../enums";
-import TiptapEditor from "./TiptapEditor.vue";
-import SheetDisplay from "./SheetDisplay.vue";
 import ShareButton from "./ShareButton.vue";
+import SheetDisplay from "./SheetDisplay.vue";
+import TiptapEditor from "./TiptapEditor.vue";
 
 export default defineComponent({
   name: "EditView",
   components: {
+    MoreButton,
     ShareButton,
     SheetDisplay,
     TiptapEditor,
---
+++ vue/src/components/MoreButton.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="dropdown" :class="{ 'is-active': more }">
+    <div class="dropdown-trigger">
+      <button
+        class="button"
+        aria-haspopup="true"
+        :aria-controls="dropdownId"
+        @click="toggle"
+      >
+        <span class="icon is-small">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            width="24"
+            height="24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            />
+          </svg>
+        </span>
+      </button>
+    </div>
+    <div class="dropdown-menu" :id="dropdownId" role="menu">
+      <div class="dropdown-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MoreButton",
+
+  props: {
+    dropdownId: {
+      type: String,
+      default: "dropdown-more",
+    },
+  },
+
+  data() {
+    return {
+      more: false,
+    };
+  },
+
+  mounted() {
+    document.addEventListener("click", this.clickHandler);
+  },
+
+  beforeUnmount() {
+    document.removeEventListener("click", this.clickHandler);
+  },
+
+  methods: {
+    toggle() {
+      this.more = !this.more;
+    },
+
+    clickHandler(event) {
+      const { target } = event;
+      const { $el } = this;
+      if (!$el.contains(target)) {
+        this.more = false;
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
