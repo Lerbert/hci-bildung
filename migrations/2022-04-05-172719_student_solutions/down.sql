442be497b57a7057205e5d2e2fd8aef25f6c8bfd
Florian Lercher <florian.lercher@tum.de>
2022-04-06 17:09:55 +0200

    Convert sheet model in migration

--- migrations/2022-04-05-172719_student_solutions/down.sql
+++ migrations/2022-04-05-172719_student_solutions/down.sql
@@ -1,3 +1,41 @@
+-- convert custom model back to tiptap
+
+CREATE FUNCTION convert_custom_mark_to_tiptap(mark JSONB)
+RETURNS JSONB
+LANGUAGE sql
+AS $$
+    SELECT jsonb_build_object('type', mark->'type')
+$$;
+
+CREATE FUNCTION convert_custom_node_to_tiptap(node JSONB)
+RETURNS JSONB
+LANGUAGE sql
+AS $$
+    SELECT jsonb_strip_nulls(jsonb_build_object(
+        'type', node->'type',
+        'content', array_to_json((
+            SELECT array_agg(convert_custom_node_to_tiptap(c))
+            FROM jsonb_array_elements(node->'content') as r(c)
+        )),
+        'marks', array_to_json((
+            SELECT array_agg(convert_custom_mark_to_tiptap(c))
+            FROM jsonb_array_elements(node->'marks') as r(c)
+        )))
+    ) || CASE node->>'type'
+            WHEN 'audio' THEN jsonb_build_object('attrs', jsonb_build_object('source', node->'source', 'mimetype', node->'mimetype'))
+            WHEN 'codeBlock' THEN jsonb_build_object('attrs', jsonb_build_object('language', node->'language'))
+            WHEN 'heading' THEN jsonb_build_object('attrs', jsonb_build_object('level', node->'level'))
+            WHEN 'multipleChoiceAnswer' THEN jsonb_build_object('attrs', jsonb_build_object('checked', node->'solution'))
+            WHEN 'text' THEN jsonb_build_object('text', node->>'text')
+            ELSE jsonb_build_object()
+        END
+$$;
+
+UPDATE sheets SET tiptap = convert_custom_node_to_tiptap(tiptap);
+
+DROP FUNCTION convert_custom_node_to_tiptap(JSONB);
+DROP FUNCTION convert_custom_mark_to_tiptap(JSONB);
+
 DROP FUNCTION delete_expired_solutions;
 
 DROP TABLE solutions;
--- migrations/2022-04-05-172719_student_solutions/up.sql
+++ migrations/2022-04-05-172719_student_solutions/up.sql
@@ -23,3 +23,47 @@ AS $$
     DELETE FROM solutions
     WHERE trashed IS NOT NULL AND trashed < NOW() - INTERVAL '30 days';
 $$;
+
+-- convert all sheets to custom model
+
+CREATE FUNCTION convert_tiptap_mark_to_custom(mark JSONB, parentNode JSONB)
+RETURNS JSONB
+LANGUAGE sql
+AS $$
+    SELECT jsonb_build_object(
+        'type', mark->'type'
+    ) || CASE mark->>'type'
+            WHEN 'gap' THEN jsonb_build_object('solution', coalesce(parentNode->>'text', ''), 'answer', '')
+            WHEN 'latex' THEN jsonb_build_object('source', coalesce(parentNode->>'text', ''))
+            ELSE jsonb_build_object()
+        END
+$$;
+
+CREATE FUNCTION convert_tiptap_node_to_custom(node JSONB)
+RETURNS JSONB
+LANGUAGE sql
+AS $$
+    SELECT jsonb_build_object(
+        'type', node->'type',
+        'content', array_to_json((
+            SELECT coalesce(array_agg(convert_tiptap_node_to_custom(c)), ARRAY[]::JSONB[])
+            FROM jsonb_array_elements(node->'content') as r(c)
+        )),
+        'marks', array_to_json((
+            SELECT coalesce(array_agg(convert_tiptap_mark_to_custom(c, node)), ARRAY[]::JSONB[])
+            FROM jsonb_array_elements(node->'marks') as r(c)
+        ))
+    ) || CASE node->>'type'
+            WHEN 'audio' THEN jsonb_build_object('source', node->'attrs'->>'source', 'mimetype', node->'attrs'->>'mimetype')
+            WHEN 'codeBlock' THEN jsonb_build_object('language', coalesce(node->'attrs'->>'language', 'plain'))
+            WHEN 'heading' THEN jsonb_build_object('level', node->'attrs'->>'level')
+            WHEN 'multipleChoiceAnswer' THEN jsonb_build_object('solution', coalesce((node->'attrs'->>'checked')::boolean, FALSE), 'answer', FALSE)
+            WHEN 'text' THEN jsonb_build_object('text', node->>'text')
+            ELSE jsonb_build_object()
+        END
+$$;
+
+UPDATE sheets SET tiptap = convert_tiptap_node_to_custom(tiptap);
+
+DROP FUNCTION convert_tiptap_node_to_custom(JSONB);
+DROP FUNCTION convert_tiptap_mark_to_custom(JSONB, JSONB);
